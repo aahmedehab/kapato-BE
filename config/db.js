b68e1113861f3362e4c052168655e669450e0300
aahmedehab <ahmedehabahmed2004@gmail.com>
2026-07-06 02:12:43 +0300

    Prepare backend for Vercel

--- config/db.js
+++ config/db.js
@@ -1,14 +1,39 @@
-const { Pool } = require('pg');
-const dotenv = require('dotenv');
+// const { Pool } = require("pg");
+// const dotenv = require("dotenv");
+
+// dotenv.config();
+
+// const pool = new Pool({
+//   host: process.env.DB_HOST,
+//   port: process.env.DB_PORT,
+//   database: process.env.DB_NAME,
+//   user: process.env.DB_USER,
+//   password: process.env.DB_PASSWORD,
+
+//   // SSL هيشتغل بس في Production
+//   ssl: process.env.NODE_ENV === "production"
+//     ? { rejectUnauthorized: false }
+//     : false,
+// });
+
+// pool.connect()
+//   .then(() => console.log("✅ Connected to PostgreSQL"))
+//   .catch((err) => console.error("❌ Database connection error:", err));
+
+// module.exports = pool;
+
+
+
+const { Pool } = require("pg");
+const dotenv = require("dotenv");
 
 dotenv.config();
 
 const pool = new Pool({
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  database: process.env.DB_NAME,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 pool.connect()
