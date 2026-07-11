9e54e3cf1c2bd6eb10f6981acf3181803319dda4
aahmedehab <ahmedehabahmed2004@gmail.com>
2026-07-11 19:41:13 +0300

    productDetails

--- models/Product.js
+++ models/Product.js
@@ -1,15 +1,44 @@
-const db = require('../config/db');
+const db = require("../config/db");
 
 const Product = {
   getAll: async () => {
-    const result = await db.query('SELECT * FROM products ORDER BY id DESC');
+    const result = await db.query("SELECT * FROM products ORDER BY id DESC");
     return result.rows;
   },
 
   getById: async (id) => {
-    const result = await db.query('SELECT * FROM products WHERE id = $1', [id]);
-    return result.rows[0];
-  }
+    const product = await db.query(
+      "SELECT * FROM products WHERE id = $1",
+      [id]
+    );
+
+    if (product.rows.length === 0) {
+      return null;
+    }
+
+const variants = await db.query(
+  `
+  SELECT
+    pv.id,
+    pv.product_id,
+    pv.sku,
+    pv.image,
+    pv.stock,
+    c.name AS color_name,
+    c.hex_code
+  FROM product_variants pv
+  JOIN colors c
+    ON pv.color_id = c.id
+  WHERE pv.product_id = $1
+  `,
+  [id]
+);
+
+    return {
+      product: product.rows[0],
+      variants: variants.rows,
+    };
+  },
 };
 
 module.exports = Product;
--- routes/products.js
+++ routes/products.js
@@ -1,13 +1,13 @@
 const express = require('express');
-const db = require('../config/db');
+const Product = require("../models/Product");
 
 const router = express.Router();
 
 // Get all products
 router.get('/', async (req, res) => {
   try {
-    const result = await db.query('SELECT * FROM products ORDER BY id DESC');
-    res.json(result.rows);
+    const products = await Product.getAll();
+    res.json(products);
   } catch (err) {
     console.error(err);
     res.status(500).json({ error: 'Server error' });
@@ -15,16 +15,22 @@ router.get('/', async (req, res) => {
 });
 
 // Get single product
-router.get('/:id', async (req, res) => {
+router.get("/:id", async (req, res) => {
   try {
-    const result = await db.query('SELECT * FROM products WHERE id = $1', [req.params.id]);
-    if (result.rows.length === 0) {
-      return res.status(404).json({ error: 'Product not found' });
+    const product = await Product.getById(req.params.id);
+
+    if (!product) {
+      return res.status(404).json({
+        error: "Product not found",
+      });
     }
-    res.json(result.rows[0]);
+
+    res.json(product);
   } catch (err) {
     console.error(err);
-    res.status(500).json({ error: 'Server error' });
+    res.status(500).json({
+      error: "Server error",
+    });
   }
 });
 
